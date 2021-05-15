92eada3ab6f428437212c85e902d3f543eedc9d4
Vyacheslav Myskov <slmyskov@gmail.com>
2021-05-15 22:44:06 +0300

    fix: fix icons size

--- src/code.ts
+++ src/code.ts
@@ -3,18 +3,14 @@ figma.showUI(__html__, {
     height: 350,
 });
 
-const ICON_WIDTH = 22;
-const ICON_HEIGHT = 24;
-
 figma.ui.onmessage = async (msg) => {
     const { values } = msg;
     const icon = figma.createNodeFromSvg(values.content);
 
     icon.name = values.name;
     icon.backgrounds = [{ type: 'SOLID', opacity: 0, color: { r: 0, g: 0, b: 0 } }];
-    icon.x = figma.viewport.center.x + ICON_WIDTH;
-    icon.y = figma.viewport.center.y + ICON_HEIGHT;
-    icon.resize(ICON_WIDTH, ICON_HEIGHT);
+    icon.x = figma.viewport.center.x;
+    icon.y = figma.viewport.center.y;
 
     figma.currentPage.appendChild(icon);
     figma.closePlugin();
